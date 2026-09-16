a65d7cf2bfe4025ad7af9e37f9627c02147f6987
radityobw <155155675+radityobw@users.noreply.github.com>
2026-09-16 16:06:51 +0700

    Refactor: remove redundant gui.py & run_gui.bat, enhance run_sidik.bat

--- run_gui.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title SIDIK - Secret Identification and Dependency Inspection Kit
-cd /d "%~dp0"
-python sidik_gui.py
-pause
--- run_sidik.bat
+++ run_sidik.bat
@@ -1,5 +1,10 @@
 @echo off
 title SIDIK - Secret Identification and Dependency Inspection Kit
 cd /d "%~dp0"
-python sidik_gui.py
-pause
+
+if "%~1"=="" (
+    python sidik_gui.py
+    pause
+) else (
+    python sidik.py %*
+)
